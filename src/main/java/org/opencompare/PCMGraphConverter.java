87233eb14ea910bad75e01b39fda04a09b7f0796
Nitramsky <jeremie.martin.prg@gmail.com>
2016-05-13 11:19:25 +0200

    ajout de getNameList
    -> créée une liste avec tous les noms de paramètres (colonnes) ex :
    poids, tailles...

--- src/main/java/org/opencompare/PCMGraphConverter.java
+++ src/main/java/org/opencompare/PCMGraphConverter.java
@@ -1,14 +1,16 @@
 package org.opencompare;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.opencompare.api.java.Cell;
 import org.opencompare.api.java.PCM;
 import org.opencompare.api.java.PCMContainer;
 import org.opencompare.api.java.Product;
-import org.opencompare.api.java.Value;
-import org.opencompare.api.java.impl.value.IntegerValueImpl;
-import org.opencompare.api.java.impl.value.RealValueImpl;
+import org.opencompare.api.java.impl.io.KMFJSONLoader;
+import org.opencompare.api.java.io.PCMLoader;
 
 public abstract class PCMGraphConverter {
 
@@ -54,7 +56,7 @@ public abstract class PCMGraphConverter {
 		boolean returnVal = false;
 		
 		// On vérifie que les paramètres correspondent à des valeurs comparables
-		if (isComparable(x) && isComparable(y) && isComparable(color) && isComparable(size) ){
+		if (isComparable(x) && isComparable(x) && isComparable(x) && isComparable(x) ){
 			returnVal = true;
 		}
 		
@@ -62,23 +64,11 @@ public abstract class PCMGraphConverter {
 	}
 	
 	private boolean isComparable(int column){
-		boolean bool = false;
-		PCM pcm = this.getPcmContainer().getPcm();
-
-        // Find the cell corresponding to the current feature and product
-        Product product = pcm.getProducts().get(0);
-        Cell cell = product.findCell(pcm.getConcreteFeatures().get(column));
-		Value interpretation = cell.getInterpretation();
-        String classValue = interpretation.getClass().getName();
-        //System.out.println(classValue);
-        if ((classValue.equals(IntegerValueImpl.class.getName())) || (classValue.equals(RealValueImpl.class.getName())))
-        	bool = true;
-        return bool;
+		// teste si la colonne peut etre utilisable pour un graphe
+        return true;
 	}
 	
 	public String getGraphData(){
-		
-		//String json = new Gson().toJson(foo );
 		// retourne les données pour affichage dans un graphe : au format JSON
 		return null;
 	}
@@ -86,5 +76,43 @@ public abstract class PCMGraphConverter {
 	public void generateHtmlFile(String file) throws IOException{
 		// génération sauvage du fichier Html
 	}
+	
+	
+	
+	// ajoute par jeremie
+	// rend la liste de parametres des produits d'un .pcm
+	public List<String> getNameList(File pcmFile) throws IOException	{
+		
+		//ancien appel dans gettingStartedTest.java
+		//File pcmFile = new File("pcms/example.pcm");
+
+		// Create a loader that can handle the file format
+		PCMLoader loader = new KMFJSONLoader();
+
+		// Load the file
+		// A loader may return multiple PCM containers depending on the input format
+		// A PCM container encapsulates a PCM and its associated metadata
+		List<PCMContainer> pcmContainers = loader.load(pcmFile);
+	
+		//resultat dans product > feature > getname
+		List<String> nameList = new ArrayList<String>() ;
+		
+		for (PCMContainer pcmContainer : pcmContainers) {
+        	
+			// Get the PCM
+			PCM pcm = pcmContainer.getPcm();	
+            
+			Product pcmProd = pcm.getProducts().get(0); 
+			for (Cell pcmCell : pcmProd.getCells()){
+				String nomCourant = pcmCell.getFeature().getName();
+				nameList.add(nomCourant);
+				//System.out.println(pcmCell.getFeature().getName());
+			}
+		}
+		//essai affichage 
+		//System.out.println(nameList.toString());
+		return nameList;
+	
+	}; //getNameList - fin
 	
 }
