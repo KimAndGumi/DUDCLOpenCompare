445be507a6ec2b78bb4b347197ce94f565cd37f6
TotoPower <TotoPower@192.168.1.21>
2016-05-17 22:53:47 +0200

    Graphe nvd3, ajout des légendes + diverses modifications

--- html/main.js
+++ html/main.js
@@ -11,28 +11,51 @@ else return(false);
 }
 var jsonString = Fichier('file.json');
 var jsonVariable = JSON.parse(jsonString);
-var data = [{
-x:JSON.parse(jsonVariable.x),
-y:JSON.parse(jsonVariable.y),
-text:jsonVariable.text,
-mode: 'markers',
-marker: {
-size:JSON.parse(jsonVariable.size),
-cmin: JSON.parse(jsonVariable.minColor),
-cmax: JSON.parse(jsonVariable.maxColor),
-colorscale: [[0,'rgb(100,50,24)'],[1,'rgb(56,100,33)']],
-showscale : true,
-color:JSON.parse(jsonVariable.color),
-} }]
-var layout = {
-title: 'Comparison_of_Nikon_DSLR_cameras',
-xaxis: {	
-	title: jsonVariable.label_x
-},
-yaxis: {	
-	title: jsonVariable.label_y
-},
-}
-TESTER = document.getElementById('tester');
-Plotly.plot( TESTER, data,layout);
-{margin: { t:0 }};
+console.log(jsonVariable);	nv.utils.symbolMap.set('thin-x', function(size) {	
+		size = Math.sqrt(size);							
+		return 'M' + (-size/2) + ',' + (-size/2) +		
+		'l' + size + ',' + size +						
+		'm0,' + -(size) +								
+		'l' + (-size) + ',' + size;						
+	});													
+	var chart;									
+	nv.addGraph(function() {					
+		chart = nv.models.scatterChart()		
+		.showDistX(true)						
+		.showDistY(true)						
+		.useVoronoi(true)						
+		.color(d3.scale.category10().range())	
+		.duration(300)							
+	;											
+	chart.dispatch.on('renderEnd', function(){	
+	console.log('render complete');				
+	});											
+	chart.xAxis.tickFormat(d3.format('.02f'));	
+	chart.yAxis.tickFormat(d3.format('.02f'));	
+	chart.xAxis.axisLabel(jsonVariable.label_x);					
+	chart.yAxis.axisLabel(jsonVariable.label_y);					
+	d3.select('#test1 svg')						
+	.datum(randomData(4,40))					
+	.call(chart);								
+	nv.utils.windowResize(chart.update);		
+	chart.dispatch.on('stateChange', function(e) { ('New State:', JSON.stringify(e)); });	
+		return chart;							
+	});											
+	function randomData(groups, points) { //# groups,# points per group	
+		var data = [],	
+		shapes = ['thin-x', 'circle', 'cross', 'triangle-up', 'triangle-down', 'diamond', 'square'],	
+		random = d3.random.normal();	
+			data.push({					
+				key: jsonVariable.title,		
+				values: []				
+			});							
+			for (j = 0; j < jsonVariable.data.length; j++) {		
+				data[0].values.push({								
+					x: JSON.parse(jsonVariable.data[j].x),			
+					y: JSON.parse(jsonVariable.data[j].y),			
+					size: JSON.parse(jsonVariable.data[j].size),	
+					shape: shapes[1]								
+				});													
+			}														
+		return data;												
+	}																
